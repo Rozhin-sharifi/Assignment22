6a279c3362bea0b612047f94224f25d8281a4198
Rozhin-sharifi <80633561+Rozhin-sharifi@users.noreply.github.com>
2021-06-30 09:31:35 +0430

    Update 22.6.py

--- 22.6.py
+++ 22.6.py
@@ -8,18 +8,13 @@ b=cv2.imread("Assignment22/hw2/f.tif")
 a=cv2.cvtColor(a,cv2.COLOR_RGB2GRAY)
 b=cv2.cvtColor(b,cv2.COLOR_RGB2GRAY)
 
-(h,w)=b.shape
-center=(w/2,h/2)
-angle=180
-scale=1
-M = cv2.getRotationMatrix2D(center, angle, scale)
-b = cv2.warpAffine(b, M, (w, h))
+b=cv2.flip(b,1)
+
 
 b=cv2.resize(b,[512,775])
 print(b.shape)
 
 img_result=a/b
-
 
 
 cv2.imwrite('finalpic.tif',img_result)
